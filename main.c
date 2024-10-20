aed62f13f99c40cb21568aab502c716517f2f4f4
jaysprogram <107363449+jaysprogram@users.noreply.github.com>
2024-10-20 14:08:48 -0400

    start

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define pi 3.14159265359
+
+
+int main(){
+    int motes = 0;
+    int device = 0;
+    double * motesVolume;
+    double * deviceVolume;
+
+
+    getInputAndCalculate(&motes, &device, );
+
+}
